3ff464b594e830f2ffe983ca1dec007ba9ad3219
AxelCardinaels <axel.cardinaels@gmail.com>
2015-01-21 22:44:44 +0100

    Vérifs formulaires

--- js/localstorage.js
+++ js/localstorage.js
@@ -11,13 +11,21 @@
     var tableStorage = new Array();
     var addPanier;
 
-    if( document.querySelector(".panier")){
+    if(document.querySelector(".panier")){
         if(localStorage.getItem('nom') == null){
             hidePanier();
         }
     }
 
+    if(document.querySelector('.errors__close')){
+        var closeBouton = document.querySelector(".errors__close");
+        closeBouton.addEventListener("click",closeError,false);
+    }
      
+    if(document.querySelector(".login__bouton")){
+        var loginBouton = document.querySelector(".login__bouton");
+        loginBouton.addEventListener("click",noAccount,false);
+    }
 
     if(document.querySelector(".validation__infos")){
         var registerBouton = document.querySelector(".validation__infos");
@@ -33,6 +41,12 @@
         var panierBouton = document.querySelector(".panier__send");
         panierBouton.addEventListener("click",addPanier,false);
     }
+
+     if(document.querySelector(".card__bouton")){
+        var panierCarte = document.querySelector(".card__bouton");
+        panierCarte.addEventListener("click",cardTest,false);
+    }
+    
     
     if(document.querySelector(".need__all")){
         setInfos();
@@ -65,7 +79,10 @@ function addPanier(){
     var img = "../img/large/chemises/chemise1a.png";
     var nombre = Number(document.querySelector('.panier__nombre').innerHTML)+ 1;
 
-    tableStorage.push( { 'nom': nom, 'type': type, 'prix': prix, 'taille': taille, 'img' : img, 'quantite' : nombre} );
+    if(taille == "Choisissez votre taille"){
+         document.querySelector('.errors__message').style.display = "block";
+    }else{
+        tableStorage.push( { 'nom': nom, 'type': type, 'prix': prix, 'taille': taille, 'img' : img, 'quantite' : nombre} );
 
     localStorage.setItem('nom',nom);
     localStorage.setItem('type',type);
@@ -76,6 +93,9 @@ function addPanier(){
 
 
     setPanierNombre();
+    }
+
+    
 
 }
 
@@ -129,14 +149,26 @@ function saveUser(){
     var ville = document.querySelector('#ville').value;
     var postal = document.querySelector('#postal').value;
 
-    localStorage.setItem('nomUser',nomUser);
-    localStorage.setItem('prenomUser',prenomUser);
-    localStorage.setItem('telephone',telephone);
-    localStorage.setItem('rue',rue);
-    localStorage.setItem('pays',pays);
-    localStorage.setItem('ville',ville);
-    localStorage.setItem('postal',postal);
+    if(checkInfos() == false){
+
+        document.querySelector('.errors__div').style.display = "block";
+        
+
+
+    }else{     
+        localStorage.setItem('nomUser',nomUser);
+        localStorage.setItem('prenomUser',prenomUser);
+        localStorage.setItem('telephone',telephone);
+        localStorage.setItem('rue',rue);
+        localStorage.setItem('pays',pays);
+        localStorage.setItem('ville',ville);
+        localStorage.setItem('postal',postal);
+        document.getElementById("formuser").action ="step3.html";
+        document.getElementById("formuser").submit();
+
+    }
 }
+
 
 function setUser(){
         document.querySelector('.user__name').innerHTML = localStorage.getItem('prenomUser') + " " + localStorage.getItem('nomUser');
@@ -154,6 +186,64 @@ function hidePanier(){
     document.querySelector(".panier__list").style.display = "none";
     document.querySelector(".panier__empty").style.display = "block";
 }
-   
+
+
+function noAccount(){
+       
+    document.querySelector(".errors__account").style.display = "block";
+}
+
+function checkInfos(){
+    var nomUser = document.querySelector('#name').value;
+    var prenomUser = document.querySelector('#surname').value;
+    var telephone = document.querySelector('#telephone').value;
+    var rue = document.querySelector('#rue').value;
+    var pays = document.querySelector('#pays').value;
+    var ville = document.querySelector('#ville').value;
+    var postal = document.querySelector('#postal').value;
+
+
+
+    if (nomUser===null || nomUser==="",prenomUser===null || prenomUser==="",telephone===null || telephone==="",rue===null || rue==="",pays==="Choisissez votre Pays" || pays==="",ville===null || ville==="",postal===null || postal==="")
+      {
+      
+      return false;
+      }
+}
+
+function cardTest(){
+     if(checkCard() == false){
+
+        document.querySelector('.errors__div').style.display = "block";
+        
+
+
+    }else{     
+        document.getElementById("formcard").action ="step4.html";
+        document.getElementById("formcard").submit();
+
+    }
+}
+
+
+function checkCard(){
+    var nomCarte = document.querySelector('#name-card').value;
+    var numCarte = document.querySelector('#number-card').value;
+    var monCarte = document.querySelector('#month-card').value;
+    var dayCarte = document.querySelector('#day-card').value;
+    var cvv = document.querySelector('#cvv-card').value;
+
+
+
+    if (nomCarte===null || nomCarte==="",numCarte===null || numCarte==="",monCarte===null || monCarte==="",dayCarte===null || dayCarte==="",cvv===null || cvv==="")
+      {
+      
+      return false;
+      }
+}
+
+function closeError(){
+    document.querySelector('.errors__div').style.display = "none";
+}
 
 } ) (jQuery);
